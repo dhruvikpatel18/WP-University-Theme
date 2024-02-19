d2dd25d66c8940a123f3e9a67a63bec390f27f47
Dhruvik Patel <dhruvikmalaviya1718@gmail.com>
2024-02-19 17:12:08 +0530

    Added active links to Navbar

--- header.php
+++ header.php
@@ -31,13 +31,37 @@
 			<div class="site-header__menu group">
 				<!-- Main Navigation -->
 				<nav class="main-navigation">
-					<ul>
-						<li><a href="<?php echo site_url( '/about-us' ); ?>">About Us</a></li>
-						<li><a href="#">Programs</a></li>
-						<li><a href="#">Events</a></li>
-						<li><a href="#">Campuses</a></li>
-						<li><a href="#">Blog</a></li>
-					</ul>
+				<ul>
+					<!-- About Us Menu Item -->
+					<li 
+					<?php
+					if ( is_page( 'about-us' ) or wp_get_post_parent_id( 0 ) == 16 ) {
+						echo 'class="current-menu-item"';}
+					?>
+					>
+						<a href="<?php echo site_url( '/about-us' ); ?>">About Us</a>
+					</li>
+
+					<!-- Programs Menu Item -->
+					<li><a href="#">Programs</a></li>
+
+					<!-- Events Menu Item -->
+					<li><a href="#">Events</a></li>
+
+					<!-- Campuses Menu Item -->
+					<li><a href="#">Campuses</a></li>
+
+					<!-- Blog Menu Item -->
+					<li 
+					<?php
+					if ( get_post_type() == 'post' ) {
+						echo 'class="current-menu-item"';}
+					?>
+					>
+						<a href="<?php echo site_url( '/blog' ); ?>">Blog</a>
+					</li>
+				</ul>
+
 				</nav>
 
 				<!-- Utility Links (Login, Sign Up, Search) -->
